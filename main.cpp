259ffddb081c4027169b99d82ec4b4630ff7ef46
KineOdegardHanssen <kineoha@student.matnat.uio.no>
2016-12-02 15:43:12 +0100

    Now traversing randomly
    Got an error when traversing grid point after grid point. Program not tested after changes.

--- main.cpp
+++ main.cpp
@@ -24,9 +24,9 @@ int main()
     int N = pow(L, dim);  // Number of sites   
     int mcsteps = 1000;   // Number of Monte Carlo steps
     int bins = 100;       // Number of bins
-    double J = 0.2;       // Strength of interaction
-    double beta = 0;    // Temperature/ Initializing beta
-    int Nbetas = 50;     // Number of different temperatures probed
+    double J = 1;         // Strength of interaction
+    double beta;          // Temperature/ Initializing beta
+    int Nbetas = 100;     // Number of different temperatures probed
     double betamin = 0;   // Lowest temperature
     double betamax = 0.1;  // Highest temperature
     double s = 0.5;       // Size of spin
@@ -38,10 +38,15 @@ int main()
 
     // Opening a file to print to
     ofstream printFile;
-    string filenamePrefix = "testing_beta0to0p1_Nbetas50_mcsteps1000_bins100";
+    string filenamePrefix = "beta0to0p1_Nbetas100_J1_mcsteps1000_bins100";
     char *filename = new char[1000];                                    // File name can have max 1000 characters
     sprintf(filename, "%s_IsingMC.txt", filenamePrefix.c_str() ); // Create filename with prefix and ending
     printFile.open(filename);
+
+    ofstream allFile;
+    char *filename2 = new char[1000];
+    sprintf(filename2, "%s_IsingMC_all.txt", filenamePrefix.c_str() ); // Create filename with prefix and ending
+    allFile.open(filename2);
 
 
     printFile << N << " " << s << " " << bins << endl;  // A line of data about the system at the top
@@ -56,11 +61,20 @@ int main()
     std::default_random_engine generator;                       // I asked the internet, and it replied
     std::uniform_real_distribution<double> distribution(0,1);
 
+    std::default_random_engine generator2;
+    std::uniform_real_distribution<int> distribution2(0,L-1);
+
     // Should I have a vector for the energies? Or use bitwise operations? Probably easier?
     // If I use bitwise operations, I could store one state in one element of a vector. But that is a lot of
     // states. So no.
 
     // Quantities for determining system state
+    double x;
+    double y;
+    double xp1;
+    double xm1;
+    double yp1;
+    double ym1;
     double prob;
     double drawn;
     double energy_new;
@@ -94,43 +108,37 @@ int main()
 
         for(int i=0; i<1000; i++)
         {  // Traversing through the spins non-randomly
-            for(int j=0; j<L;j++)
+            for(int j=0; j<N; j++)
             {
-                for(int k=0; k<L; k++)
-                {   // Know thy neighbours
-                    // But seriously, there should be a simpler way to do this...
-                    // Double check at least
+                x = distribution2(generator2);
+                y = distribution2(generator2);
+                // Periodic boundary conditions
+                if(x==(L-1))                    xp1 = 0;
+                else                            xp1 = x+1;
+                if(x==0)                        xm1 = L-1;
+                else                            xm1 = x-1;
+                if(y==(L-1))                    yp1 = 0;
+                else                            yp1 = y+1;
+                if(y==0)                        ym1 = L-1;
+                else                            ym1 = y-1;
 
-                    if(j==0)            // Plus sign in front as state(j,k) --> -state(j,k).
-                    {
-                        if(k==0)         energy_change = 2*J*state(0,k)*(state(L-1,k) + state(0,L-1) + state(0,1) + state(1,k));
-                        else if(k==L-1)  energy_change = 2*J*state(0,k)*(state(L-1,k) + state(0,k-1) + state(1,k) + state(0,0));
-                        else             energy_change = 2*J*state(0,k)*(state(L-1,k) + state(0,k-1) + state(1,k) + state(0,k+1));
-                    }
-                    if(k==0)
-                    {
-                        if(j==L-1)                     energy_change = 2*J*state(j,0)*(state(j-1,0) + state(j,L-1) + state(0,0) + state(j,1));
-                        if(j!=0 && j!=L-1)             energy_change = 2*J*state(j,0)*(state(j-1,0) + state(j,L-1) + state(j+1,0) + state(j,1));
-                    }
-                    if(j==L-1 && k!=0 && k!=L-1)       energy_change = 2*J*state(j,k)*(state(j-1,k) + state(j,k-1) + state(0,k) + state(j,k+1));
-                    if(k==L-1 && j!=0 && j!=L-1)       energy_change = 2*J*state(j,k)*(state(j-1,k) + state(j,k-1) + state(j+1,k) + state(j,0));
-                    if(j!=0 && j!=L-1 && k!=0 && k!=L-1)    energy_change = 2*J*state(j,k)*(state(j-1,k) + state(j,k-1) + state(j+1,k) + state(j,k+1));
+                energy_change = 2*J*state(x,y)*(state(xm1,y) + state(x,ym1) + state(xp1,y) + state(x,yp1));
+                energy_new = energy_curr + energy_change;
 
-                    energy_new = energy_curr + energy_change;
-                    if(energy_new <= energy_curr)
+                if(energy_new <= energy_curr)
+                {
+                    state(j,k) = -state(j,k); // Flip spin if the result is a state with lower energy.
+                    energy_curr = energy_new; // Update energy
+                }
+                else
+                {
+                    prob = exp(-beta*(energy_new-energy_curr));
+                    drawn = distribution(generator);
+                    if(drawn<prob)
                     {
-                        state(j,k) = -state(j,k); // Flip spin if the result is a state with lower energy.
-                        energy_curr = energy_new; // Update energy
-                    }
-                    else
-                    {
-                        prob = exp(-beta*(energy_new-energy_curr));
-                        drawn = distribution(generator);
-                        if(drawn<prob)
-                        {
-                            state(j,k) = -state(j,k);  // Flip spin.
-                            energy_curr = energy_new;  // Update energy
-                        }
+
+                        state(j,k) = -state(j,k);  // Flip spin.
+                        energy_curr = energy_new;  // Update energy
                     }
                 }
             }
@@ -144,7 +152,7 @@ int main()
         vec msq_av_bin = zeros(bins);
         double m = 0;
 
-        double counter = 0;
+        //double counter = 0;
         for(int l=0; l<bins; l++)
         {   // Loop over the bins
             msq_av_bin(l) = 0;
@@ -154,45 +162,37 @@ int main()
                 // Traversing through the spins non-randomly
                 for(int j=0; j<L;j++)
                 {
-                    for(int k=0; k<L; k++)
-                    {   // Know thy neighbours
-                        // But seriously, there should be a simpler way to do this...
-                        // Double check at least
-                        // Lots of steps to avoid segmentation faults
-                        // + enforcing boundary conditions
-                        if(j==0)            // Plus sign in front as state(j,k) --> -state(j,k).
-                        {
-                            if(k==0)         energy_change = 2*J*state(0,k)*(state(L-1,k) + state(0,L-1) + state(0,k+1) + state(1,k));
-                            else if(k==L-1)  energy_change = 2*J*state(0,k)*(state(L-1,k) + state(0,k-1) + state(0,0)   + state(1,k));
-                            else             energy_change = 2*J*state(0,k)*(state(L-1,k) + state(0,k-1) + state(0,k+1) + state(1,k));
-                        }
-                        if(k==0)
-                        {
-                            if(j==L-1)                     energy_change = 2*J*state(j,0)*(state(j-1,0) + state(j,L-1) + state(0,0)   + state(j,1));
-                            if(j!=0 && j!=L-1)             energy_change = 2*J*state(j,0)*(state(j-1,0) + state(j,L-1) + state(j+1,0) + state(j,k+1));
-                        }
-                        if(j==L-1 && k!=0 && k!=L-1)       energy_change = 2*J*state(j,k)*(state(j-1,k) + state(j,k-1) + state(0,k)   + state(j,k+1));
-                        if(k==L-1 && j!=0 && j!=L-1)       energy_change = 2*J*state(j,k)*(state(j-1,k) + state(j,k-1) + state(j+1,k) + state(j,0));
-                        if(j!=0 && j!=L-1 && k!=0 && k!=L-1)    energy_change = 2*J*state(j,k)*(state(j-1,k) + state(j,k-1) + state(j+1,k) + state(j,k+1));
+                    x = distribution2(generator2);
+                    y = distribution2(generator2);
+                    // Periodic boundary conditions
+                    if(x==(L-1))                    xp1 = 0;
+                    else                            xp1 = x+1;
+                    if(x==0)                        xm1 = L-1;
+                    else                            xm1 = x-1;
+                    if(y==(L-1))                    yp1 = 0;
+                    else                            yp1 = y+1;
+                    if(y==0)                        ym1 = L-1;
+                    else                            ym1 = y-1;
 
+                    energy_change = 2*J*state(x,y)*(state(xm1,y) + state(x,ym1) + state(xp1,y) + state(x,yp1));
+                    energy_new = energy_curr + energy_change;
 
-                        energy_new = energy_curr + energy_change;
-                        if(energy_new <= energy_curr)
-                        { // If the new energy is lower than the energy of the old, we commit the change
-                            state(j,k) = -state(j,k); // Flip spin if the result is a state with lower energy.
-                            energy_curr = energy_new; // Update energy
+                    energy_new = energy_curr + energy_change;
+                    if(energy_new <= energy_curr)
+                    { // If the new energy is lower than the energy of the old, we commit the change
+                        state(j,k) = -state(j,k); // Flip spin if the result is a state with lower energy.
+                        energy_curr = energy_new; // Update energy
+                    }
+                    else
+                    { // If the new energy is larger than the energy of the old, we leave it to chance
+                        prob = exp(-beta*(energy_new-energy_curr));
+                        drawn = distribution(generator);
+                        if(drawn<prob)
+                        {
+                            state(j,k) = -state(j,k);  // Flip spin.
+                            energy_curr = energy_new;  // Update energy
                         }
-                        else
-                        { // If the new energy is larger than the energy of the old, we leave it to chance
-                            prob = exp(-beta*(energy_new-energy_curr));
-                            drawn = distribution(generator);
-                            if(drawn<prob)
-                            {
-                                state(j,k) = -state(j,k);  // Flip spin.
-                                energy_curr = energy_new;  // Update energy
-                            }
-                        } // End if-tests of energy
-                    } // End loop over k
+                    } // End if-tests of energy
                 } // End loop over j. All lattice points have been traversed over
                 //Commands to find quantity
                 m = 0;    // Resetting for this run
@@ -202,15 +202,13 @@ int main()
                 }  // Finding the desired quantity
 
                 double yo = m*m/(N*N); // Man gave name to all the quantities. In the beginning. In the beginning.
-                msq_av_bin(l) += yo;   // Should I maybe take the absolute value instead?
+                msq_av_bin(l) += yo;   // Accumulating the average for bin l
                 binholder_m(l,i)= yo;  // Save each value of m grouped by bin.
             }  // End of Monte Carlo steps. Index i out.
-
-            msq_av_bin(l) = msq_av_bin(l)/mcsteps; // Dividing by the number of configurations.
+            // Dividing by the number of configurations to get the average for bin l
+            msq_av_bin(l) = msq_av_bin(l)/mcsteps;
 
         }  // End of loop over bins. Index l out.
-
-        // Have print commands here instead.
 
         // Calculating the average magnetization over all bins.
         m_average = 0;
@@ -219,15 +217,18 @@ int main()
 
         // Variance, according to MHJ
         variance = 0;
+        // Loop for accumulating terms for the variance
         for(int i=0; i<bins; i++)
         {
             for(int k=0; k<mcsteps; k++)    variance += (binholder_m(i,k)-m_average)*(binholder_m(i,k)-m_average);
         }
+        // Dividing to get the variance
         variance = variance/(bins*mcsteps);
 
 
         // Finding the covariance term in sigma_m, according to MHJ:
         covariance_term = 0;
+        // Loop for accumulating terms for the covariance term
         for(int i=0; i<bins; i++)
         {
             for(int k=0; k<mcsteps; k++)
@@ -238,19 +239,20 @@ int main()
                     covariance_term += (binholder_m(i,k)-m_average)*(binholder_m(i,p)-m_average);
                     /*if(hi<0)
                     {
-                        cout << "Negative contribution to  the covariance term" << endl;
+                        // cout << "Negative contribution to  the covariance term" << endl;
                         counter++;
-                        cout << "This is the " << counter << "th time" << endl;
+                        //cout << "This is the " << counter << "th time" << endl;
                     }*/
                 }
             }
         }
+        // Multiplying to get the covariance term
         covariance_term = covariance_term*2/(bins*mcsteps*mcsteps);
 
         // Sigma_m^2, according to MHJ
         sigma_msq = variance/mcsteps + covariance_term;
 
-        // Autocorrelation time
+        // Autocorrelation time, according to MHJ
         autocorrelation_time = 0;
         for(int i=0; i<bins; i++)
         {
@@ -265,11 +267,26 @@ int main()
         if(sigma_msq==0 && variance==0 && covariance_term==0)    autocorrelation_time = 1; // Correlation free experiment: autocorrelation_time=1
         // Get infinity when variance=0, but when covariance=0, we have a correlation-free experiment.
 
-        printFile << beta << " " << m_average << " " << sigma_msq << " " << variance << " " << covariance_term << " " << autocorrelation_time  << endl;
+        // What the others seem to be suggesting (Formula from Kosovan and Sega)
+        // This is quite similar to the variance...
+        double blockvariance = 0;
+        // Accumulating
+        for(int i=0; i<bins; i++)    blockvariance += (msq_av_bin(i)-m_average)*(msq_av_bin(i)-m_average);
+        // Dividing
+        blockvariance = blockvariance/(bins*(bins-1));
+
+
+        printFile << beta << " " << m_average << " " << sigma_msq << " " << variance << " " << covariance_term << " " << autocorrelation_time << " " << blockvariance  << endl;
+
+        //cout << "Number of negative contributions to the covariance term:" << counter << endl;
+        //counter = 0;
 
     }  // End of loop over betas. Index n out.
     delete filename;
     printFile.close(); // Closing the file
+
+    delete filename2;
+    allFile.close();
 
 } // End of main
 
