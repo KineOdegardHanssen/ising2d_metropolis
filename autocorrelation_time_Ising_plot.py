4fb8927525a44943df048eb4201bee6bde41f520
KineOdegardHanssen <kineoha@student.matnat.uio.no>
2016-12-06 12:30:07 +0100

    Trying to make sense of the autocorrelation time
    Grasping at straws, I guess. Will talk to supervisor later.

--- autocorrelation_time_Ising_plot.py
+++ autocorrelation_time_Ising_plot.py
@@ -4,7 +4,7 @@ from matplotlib.pyplot import *
 import time
 import sys
 
-# Function to find the autocorrelation time
+# Function to find the autocorrelation time ...
 def corr(deltat, ms):
     lenms = len(ms)
     a = 0
@@ -21,14 +21,26 @@ def corr(deltat, ms):
             a += ms[i]*ms[i+dt]
     return a
 
-# Test to see if figure works
-x = linspace(0, 4, 100)
-y = x**2*exp(-0.1*x)
-figure()
-plot(x,y)
-show()
+def autocorrelation_time(i, Nbeta, ms):
+    lenms = len(ms)
+    a = 0
+    #upperlimitdt = int(floor(0.25*Nbeta)) + 1  # Experiment with this
+    upperlimit = (i+1)*Nbeta 
+    for j in range(i*Nbeta, upperlimit):
+        if (j+dt)>=upperlimit:
+            # Are we cyclic in time? In that case, break.
+            return a
+            # Or periodic boundary conditions? Seems weird, though...
+            #index = i+dt-lenms
+            #a += ms[i]*ms[index]
+        else:
+            # Keep accumulating the sum
+            a += ms[j]*ms[j+dt]
+    return a
 
 ## Reading in the data ##
+# Set this manually to suit the data file
+Nbeta = 100
 # Open the file for reading
 infile = open("beta0to5_Nbetas100__spin0p5_L15_J1_mcsteps1000_bins100_IsingMC_all.txt", "r")
 
@@ -65,9 +77,19 @@ endpoint = 100
 deltats = linspace(0, endpoint, endpoint+1)     # Array of distance between spins, to be plotted against correlations
 ams = zeros(endpoint+1)                         # Correlation array
 
+
 for i in range(len(deltats)):
     print deltats[i]
     ams[i]= corr(deltats[i], ms)
+
+# Sort after the value of beta
+j = 0
+betasort = linspace(0,5,Nbeta)
+actime = zeros(Nbeta)
+for i in range(lenms):
+    # Loop over dt as well?
+    actime[i] += autocorrelation_time(i, Nbeta, ms)
+    
 
 # Doing the plotting thing
 # Not integrated
